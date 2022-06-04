e194fd24ef8604946a6193ed709af66fc4ebb866
javeriatabassum145 <javeria.tabassum145@gmail.com>
2022-06-05 01:15:11 +0500

    final

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 require('colors');
 const dotenv = require('dotenv');
+const path = require('path')
 const connectDB = require('./config/config');
 
 //config dotenv
@@ -21,6 +22,17 @@ app.use(morgan('dev'))
 app.use('/api/pizzas', require('./routes/pizzaRoute'))
 app.use('/api/users', require('./routes/userRoute'))
 app.use('/api/orders', require('./routes/orderRoute'))
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"frontend","build","index.html"))
+    })
+}else{
+    app.get("/", (req,res) => {
+        res.send("<h1>Hello From Node Server Nodemon</h1>")
+    })
+}
 
 app.get('/',(req,res)=>{
     res.send("<h1>Hello From Node Server</h1>")
